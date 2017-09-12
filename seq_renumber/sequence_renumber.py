358bca94feb9f8dc8b9fb2c76a4b64de99717dd4
jaideep-khadilkar <jaideep.khadilkar@gmail.com>
2017-09-13 05:42:46 +1000

    argparse

--- seq_renumber/sequence_renumber.py
+++ seq_renumber/sequence_renumber.py
@@ -6,24 +6,26 @@ import sys
 import argparse
 import logging
 
-def SequenceRenumber(sourceDir="",padZero=0,renameUnnumberedFiles=False,recursive=False):
+def SequenceRenumber(sourceDir="",padZero=0,recursive=False):
     if not os.path.isdir(sourceDir):
         print 'Error : {0} directory does not exist !'.format(sourceDir)
         return
+
+    if(padZero<0):
+        print 'Error : Zero padding value should not be negative'
+        return
+
     items = os.listdir(sourceDir)
     items.sort()
     sequenceMap = {}
-    
+
     for item in items:
         fullPath = os.path.join(sourceDir,item)
         if os.path.isfile(fullPath):
-            if(renameUnnumberedFiles):
-                pattern = r"(?P<name>[a-zA-Z0-9_]+[a-zA-Z_]+)(?P<number>[0-9]*)\.(?P<extension>[a-zA-Z0-9]+)"
-            else:
-                pattern = r"(?P<name>[a-zA-Z0-9_]+[a-zA-Z_]+)(?P<number>[0-9]+)\.(?P<extension>[a-zA-Z0-9]+)"
+            pattern = r"(?P<name>[a-zA-Z0-9_]+[a-zA-Z_]+)(?P<number>[0-9]+)\.(?P<extension>[a-zA-Z0-9]+)"
             match = re.match(pattern,item)
             if(not match):
-                print 'Warning : Skipping item {0}. It does not fit into the rename rules'.format(item)
+                print 'Warning : Skipping item {0}. It does not fit into the renaming rules'.format(item)
                 continue
             name = match.group('name')
             number = match.group('number')
@@ -39,7 +41,7 @@ def SequenceRenumber(sourceDir="",padZero=0,renameUnnumberedFiles=False,recursiv
         else:
             if os.path.isdir(fullPath):
                 if recursive:
-                    SequenceRenumber(fullPath, padZero, renameUnnumberedFiles, recursive)
+                    SequenceRenumber(fullPath, padZero, recursive)
                 else:
                     print 'Warning : Skipping directory {0}'.format(item)
                     continue
@@ -47,24 +49,26 @@ def SequenceRenumber(sourceDir="",padZero=0,renameUnnumberedFiles=False,recursiv
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s","--Source_Directory_Path",dest='sourceDir',default=None,nargs='+',
-                        help="The source directory on which the renumber is to be executed")
-    parser.add_argument("-z","--Zero_Padding",dest='padZero',default=None,nargs='?',
+    parser.add_argument("sourceDir",default=None,metavar='SOURCE_DIRECTORY',
+                        help="Absolute or relative path to the source directory on which the renumber is to be executed")
+    parser.add_argument("-z","--Zero_Padding",dest='padZero',default=None,required=False,metavar='N',
                         help="Number of zeros to pad to the resultant sequence")
-    parser.add_argument('-r', "--Recursive",dest='recursive',action='store_true')
+    parser.add_argument('-r', "--Recursive",dest='recursive',action='store_true',default=None,required=False,
+                        help="Recursively apply operation to the children directories.")
     
     opts = parser.parse_args()
     if not opts.sourceDir:
         print "Error : Please specify the source directory"
         sys.exit(1)
-    sourceDir = opts.sourceDir[0]
      
     padZero = 0
     if opts.padZero:
-        if(opts.padZero<0):
-            print 'Error : Zero padding value shout not be negative'
-        else:
-            padZero = int(opts.padZero)
-     
-    sourceDirAbsPath = os.path.abspath(sourceDir)
-    SequenceRenumber(sourceDir=sourceDirAbsPath, padZero=padZero, renameUnnumberedFiles=False, recursive=opts.recursive)
+        padZero = int(opts.padZero)
+      
+    sourceDirAbsPath = os.path.abspath(opts.sourceDir)
+    SequenceRenumber(sourceDir=sourceDirAbsPath, padZero=padZero, recursive=opts.recursive)
+    
+    
+    
+    
+    
