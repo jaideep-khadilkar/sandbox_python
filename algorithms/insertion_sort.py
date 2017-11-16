aa03b76f2cb6283bd822221e6ccc9244b5c5168d
jaideep-khadilkar <jaideep.khadilkar@gmail.com>
2017-11-17 06:33:17 +1100

    algorithms

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,15 @@
+
+from array import array
+
+arr = array('i',[52,-100,942,0,2,23,-23,89,2,63])
+    
+for key_index in range(1,len(arr)):
+    print arr
+    key = arr[key_index]
+    slide_index = key_index-1
+    while slide_index>-1 and key<arr[slide_index]:
+        arr[slide_index+1] = arr[slide_index]
+        slide_index = slide_index -1
+    arr[slide_index+1] = key
+
+print arr
