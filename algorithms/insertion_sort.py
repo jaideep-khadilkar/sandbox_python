54aeb587145664c6e0f7604dd92146567beb588e
jaideep-khadilkar <jaideep.khadilkar@gmail.com>
2018-08-14 06:51:34 -0700

    modified insertion sort

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,18 +1,15 @@
 
 from array import array
-
+    
 def insertion_sort(arr):
-    for key_index in range(1,len(arr)):
-        print arr
-        key = arr[key_index]
+    for key_index,key in enumerate(arr):
         slide_index = key_index-1
         while slide_index>-1 and key<arr[slide_index]:
             arr[slide_index+1] = arr[slide_index]
             slide_index = slide_index -1
         arr[slide_index+1] = key
-    
     print arr
-    
 
 arr = array('i',[52,-100,942,0,2,23,-23,89,2,63])
+print arr
 insertion_sort(arr)
